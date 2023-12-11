9691afdf6c47156e43c623f2abe6b6e432af6f95
Luis P <153492793+baggie-paucity@users.noreply.github.com>
2023-12-11 16:44:15 +0700

    Create my_module.py

---
+++ my_module.py
@@ -0,0 +1,10 @@
+# my_module.py
+
+def greet(name):
+    """
+    This function takes a name as input and prints a greeting message.
+    
+    Args:
+    - name (str): The name of the person to greet.
+    """
+    print(f"Hello, {name}! Welcome to the program.")
